416feee6ec23d4e92a54a8b85b752dff15f193db
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-09-19 17:34:00 +0600

    Third commit

--- src/Component/Country/Contri/Countri.jsx
+++ src/Component/Country/Contri/Countri.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./countri.css";
 
-const handleVisited = () => {
-    console.log("button clicked");
+const Countri = ({countri, visitedCountrysHandler}) => {
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+    // setVisited(visited ? false : true)
+    setVisited(!visited)
+    visitedCountrysHandler()
 }
 
-const Countri = ({countri}) => {
-    console.log(countri.area.area);
+    // console.log(countri.area.area);
+    console.log(visitedCountrysHandler)
     return (
-        <div className='countri'>
-            <img className='style-flag' src={countri.flags.flags.png} alt={countri.flags.flags.alt} />
+        <div className={`countri ${visited && 'country-visited'}`}>
+            <img className='flag' src={countri.flags.flags.png} alt={countri.flags.flags.alt} />
             <h3>Name : {countri.name.common}</h3>
             <p>Population : {countri.population.population}</p>
             <p>Region : {countri.region.region}</p>
             <h4>Capital : {countri.capital.capital}</h4>
             <p>Area : {countri.area.area} {countri.area.area > 300000 ? "Big Country" : "Small Country"}</p>
             
-            <button onClick={handleVisited}>No visit</button>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Not Visited"}
+            </button>
         </div>
     );
 };
--- src/Component/Country/Countrys/Countrys.jsx
+++ src/Component/Country/Countrys/Countrys.jsx
@@ -1,22 +1,30 @@
-import React, { use } from 'react';
-import Countri from '../Contri/Countri';
-import './countrys.css'
+import React, { use, useState } from "react";
+import Countri from "../Contri/Countri";
+import "./countrys.css";
 
-const Countrys = ({countrysPromise}) => {
+const Countrys = ({ countrysPromise }) => {
+  const [visitedCountrys, setVisitedCountrys] = useState([]);
 
-    const countrysData = use(countrysPromise);
-    const countrys = countrysData.countries;
-    console.log(countrys);
+  const visitedCountrysHandler = () => {
+    console.log("VisitedCountrys button Clicked.");
+  };
 
-    return (
-        <div className='countrys'>
-            <h2>Countrys : {countrys.length}</h2>
+  const countrysData = use(countrysPromise);
+  const countrys = countrysData.countries;
+  console.log(countrys);
 
-            {
-                countrys.map(countri => <Countri countri = {countri}></Countri>) 
-            }
-        </div>
-    );
+  return (
+    <div className="countrys">
+      <h2>Countrys : {countrys.length}</h2>
+      <h3>Total Countrys Visited :</h3>
+
+      {countrys.map((countri) => (
+        <Countri countri={countri}
+        visitedCountrysHandler = {visitedCountrysHandler}        
+        ></Countri>
+      ))}
+    </div>
+  );
 };
 
 export default Countrys;
